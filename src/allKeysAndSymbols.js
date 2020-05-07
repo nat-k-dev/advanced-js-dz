080ad6e71ddf9297b4b182fd065e4ef738cd70cd
appalse <frontend-hh@yandex.ru>
2020-05-07 08:49:45 +0300

    replace __proto__ by getPrototypeOf

--- src/allKeysAndSymbols.js
+++ src/allKeysAndSymbols.js
@@ -6,8 +6,8 @@ function allKeysAndSymbols (object) {
     if (typeof object !== 'object') return [];
     let result = new Set([].concat(Object.getOwnPropertyNames(object), 
                            Object.getOwnPropertySymbols(object)));
-    if (object.__proto__ !== null) {
-        result = new Set(allKeysAndSymbols(object.__proto__).concat([...result]));
+    if (Object.getPrototypeOf(object) !== null) {
+        result = new Set(allKeysAndSymbols(Object.getPrototypeOf(object)).concat([...result]));
     }
     for (const field in object) {
         result = new Set(allKeysAndSymbols(object[field]).concat([...result]));
